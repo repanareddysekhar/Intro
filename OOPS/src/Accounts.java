b0b6a5585d53919a94fa1ae99f71bb960d5e7b7e
huhy <huhy@huhy-node8>
2018-07-24 12:31:01 +0530

    Modified

---
+++ OOPS/src/Accounts.java
@@ -0,0 +1,8 @@
+
+public class Accounts {
+
+	  public void processSalary(Confirmed employee){
+		  employee.netSalary();
+		  
+	  }
+}
--- OOPS/src/MainClass.java
+++ OOPS/src/MainClass.java
@@ -3,7 +3,10 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       Confirmed employee=new Confirmed();
+       Accounts accounts=new Accounts();
+       accounts.processSalary(employee);
+       employee.netSalary();
 		
 	}
 
