a632f5adcbe46fba80a4b2ad3debf09946aa1513
huhy <huhy@huhy-node8>
2018-07-24 10:35:02 +0530

    nothing changed

---
+++ Java/src/com/huhytechnologies/main/cui/MainClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.huhytechnologies.main.cui;
+
+
+public class MainClass {
+
+	
+	public static void main(String[] args) {
+		int num[];
+        num = new int[4];
+		num[1]=5;
+		for(int x: num)
+		{
+			System.out.println(x);
+		}
+		
+	}
+
+}
