c1529e576d0cb8e4207db37d7d75c1bbc8d42441
huhy <huhy@huhy-node8>
2018-07-24 12:19:21 +0530

    modified

---
+++ OOPS/src/Confirmed.java
@@ -0,0 +1,11 @@
+
+public class Confirmed extends Employee{
+
+	@Override
+	public void netSalary() {
+		// TODO Auto-generated method stub
+		System.out.println("Confirmed employee salary");
+	}
+	
+
+}
---
+++ OOPS/src/Employee.java
@@ -0,0 +1,6 @@
+
+public abstract class Employee {
+
+	 public abstract void netSalary();
+	 
+}
